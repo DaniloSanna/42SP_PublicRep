e4fb03a5c41c650ce5ddb52b6f0e0f6dcbc8d628
Danilo Sanna <66884541+DaniloSanna@users.noreply.github.com>
2022-10-05 19:34:45 -0300

    Update ft_strlcpy.c

--- Entrega/ft_strlcpy.c
+++ Entrega/ft_strlcpy.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strlcpy.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: djustino <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: djustino <djustino@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/28 19:42:11 by djustino          #+#    #+#             */
-/*   Updated: 2022/09/28 19:42:13 by djustino         ###   ########.fr       */
+/*   Updated: 2022/10/01 21:11:47 by djustino         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,3 +28,5 @@ size_t	ft_strlcpy(char *dest, const char *src, size_t size)
 	}
 	return (ft_strlen(src));
 }
+// This function copy the strings, it´s take full size of buffer
+// (not only the size) then garantee the null at end
