14b4c554d1f9c0d5d4b77f8fc13bfc56806163d7
Danilo Sanna <66884541+DaniloSanna@users.noreply.github.com>
2022-10-05 19:35:41 -0300

    Update ft_strncmp.c

--- Entrega/ft_strncmp.c
+++ Entrega/ft_strncmp.c
@@ -3,26 +3,27 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strncmp.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: djustino <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: djustino <djustino@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/28 19:42:42 by djustino          #+#    #+#             */
-/*   Updated: 2022/09/28 19:42:42 by djustino         ###   ########.fr       */
+/*   Updated: 2022/10/01 21:13:52 by djustino         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_strncmp(const char *s1, const char *s2, size_t limit)
+int	ft_strncmp(const char *array1, const char *array2, size_t limit)
 {
 	size_t	loop;
 
 	loop = 0;
 	while (loop < limit)
 	{
-		if (s1[loop] - s2[loop] == 0)
+		if (array1[loop] - array2[loop] == 0)
 			loop++;
 		else
-			return ((unsigned char)s1[loop] - (unsigned char)s2[loop]);
+			return ((unsigned char)array1[loop] - (unsigned char)array2[loop]);
 	}
 	return (0);
 }
+// This function compare both arrays and return the diference of letters.
