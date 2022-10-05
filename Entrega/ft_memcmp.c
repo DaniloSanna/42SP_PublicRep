6efc12aad1ac4ad5e35844e6ee0292f82bcb1ffe
Danilo Sanna <66884541+DaniloSanna@users.noreply.github.com>
2022-10-05 19:28:30 -0300

    Update ft_memcmp.c

--- Entrega/ft_memcmp.c
+++ Entrega/ft_memcmp.c
@@ -3,23 +3,23 @@
 /*                                                        :::      ::::::::   */
 /*   ft_memcmp.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: djustino <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: djustino <djustino@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/28 19:38:56 by djustino          #+#    #+#             */
-/*   Updated: 2022/09/28 19:38:58 by djustino         ###   ########.fr       */
+/*   Updated: 2022/10/01 21:20:02 by djustino         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_memcmp(const void *s1, const void *s2, size_t limit)
+int	ft_memcmp(const void *array1, const void *array2, size_t limit)
 {
 	const unsigned char	*castone;
 	const unsigned char	*casttwo;
 	size_t				loop;
 
-	castone = s1;
-	casttwo = s2;
+	castone = array1;
+	casttwo = array2;
 	loop = 0;
 	if (limit == 0)
 		return (0);
@@ -31,3 +31,5 @@ int	ft_memcmp(const void *s1, const void *s2, size_t limit)
 	}
 	return (0);
 }
+// This function compare one string to another untill limit,
+// and return its difference.
