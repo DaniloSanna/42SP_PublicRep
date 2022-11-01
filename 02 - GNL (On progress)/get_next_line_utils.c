63b6051253bb0d08c86631055ff53a1281c49c2f
Danilo Sanna <66884541+DaniloSanna@users.noreply.github.com>
2022-11-01 20:01:46 -0300

    Create get_next_line_utils.c

---
+++ 02 - GNL (On progress)/get_next_line_utils.c
@@ -0,0 +1,67 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: djustino <djustino@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/30 08:38:49 by djustino          #+#    #+#             */
+/*   Updated: 2022/10/30 14:53:11 by djustino         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+size_t	ft_strlen(const char *str)
+{
+	int	i;
+
+	i = 0;
+	if (!str)
+		return (0);
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	int	i;
+
+	i = 0;
+	if (c > 127)
+		c %= 256;
+	while (s[i] != '\0' && s[i] != c)
+		i++;
+	if (s[i] == c)
+		return ((char *)&s[i]);
+	return (0);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*mrg;
+	int		size;
+	int		i;
+	int		j;
+
+	size = ft_strlen((char *)s1) + ft_strlen((char *)s2);
+	mrg = malloc((size + 1) * sizeof(char));
+	if (!mrg)
+		return (NULL);
+	i = 0;
+	while (s1 && s1[i] != '\0')
+	{
+		mrg[i] = s1[i];
+		i++;
+	}
+	j = 0;
+	while (s2[j] != '\0')
+		mrg[i++] = s2[j++];
+	if (s1)
+		free ((char *)s1);
+	mrg[i] = '\0';
+	return (mrg);
+}
