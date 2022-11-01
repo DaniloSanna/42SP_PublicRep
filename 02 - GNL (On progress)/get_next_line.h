844e4b7a97a98e8ddea09260364d57523b4b3a16
Danilo Sanna <66884541+DaniloSanna@users.noreply.github.com>
2022-11-01 20:01:19 -0300

    Create get_next_line.h

---
+++ 02 - GNL (On progress)/get_next_line.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: djustino <djustino@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/30 08:38:19 by djustino          #+#    #+#             */
+/*   Updated: 2022/10/30 16:22:05 by djustino         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# include <stdlib.h>
+# include <unistd.h>
+# include <fcntl.h>
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+char		*get_next_line(int fd);
+char		*ft_strjoin(char const *s1, char const *s2);
+size_t		ft_strlen(const char *str);
+char		*ft_strchr(const char *s, int c);
+
+#endif
